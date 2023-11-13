b2887410e7f4ba3a529e474f403de8cba970cf5e
Diego Avila Ramos <145205871+avila46@users.noreply.github.com>
2023-11-13 20:37:26 +0000

    cambio de indices arreglado

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ let noun = ['jogger', 'racoon'];
 for (let index = 0; index < pronoun.length; index++) {
     for (let indexAdj = 0; indexAdj < adj.length; indexAdj++) {
         for (let indexNoum = 0; indexNoum < noun.length; indexNoum++) {
-            console.log(pronoun[indexAdj] + adj[index] + noun[indexNoum] + ".com"); 
+            console.log(pronoun[index] + adj[indexAdj] + noun[indexNoum] + ".com"); 
         }
     }
 }
